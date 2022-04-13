f9298dd765ba57eb5a4f32b1bceb68d73b6851dd
Aaron Barlow <aaron.barlow@progleasing.com>
2022-04-13 09:43:01 -0600

    add aft test

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,13 @@
-module "sandbox" {
+module "test1" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
-    AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
-    SSOUserFirstName          = "Sandbox"
-    SSOUserLastName           = "AFT"
+    AccountEmail              = "AFT-MemberTest1LAB.AcctAwsPrg@progleasing.com"
+    AccountName               = "AFT-MemberTest1LAB"
+    ManagedOrganizationalUnit = "AFT-LZ (ou-4nab-cmm3u75a)"
+    SSOUserEmail              = "AFT-MemberTest1LAB.AcctAwsPrg@progleasing.com"
+    SSOUserFirstName          = "AFT"
+    SSOUserLastName           = "MemberTest1LAB"
   }
 
   account_tags = {
@@ -15,12 +15,12 @@ module "sandbox" {
   }
 
   change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
+    change_requested_by = "AaronB"
     change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
   }
 
   custom_fields = {
-    group = "non-prod"
+    group = "non-prod1"
   }
 
   account_customizations_name = "sandbox"
