5ff6ca7eb8ac91e83480f8fc367cca08a90583dc
Aaron Barlow <aaron.barlow@progleasing.com>
2022-04-14 15:12:00 -0600

    I don't even know anymore

--- terraform/modules/aft-account-request/ddb.tf
+++ terraform/modules/aft-account-request/ddb.tf
@@ -4,15 +4,12 @@ resource "aws_dynamodb_table_item" "account-request" {
     for_each = { for combo in var.account_assignment_map : combo.AccountEmail => combo}
   item = jsonencode({
     id = each.value.AccountEmail
-    account_assignment_map = { M = {
-      AccountEmail              = each.value.AccountEmail
-      AccountName               = each.value.AccountName
-      ManagedOrganizationalUnit = each.value.ManagedOrganizationalUnit
-      SSOUserEmail              = each.value.SSOUserEmail
-      SSOUserFirstName          = each.value.SSOUserFirstName
-      SSOUserLastName           = each.value.SSOUserLastName
-      }
-    }
+    AccountEmail              = each.value.AccountEmail
+    AccountName               = each.value.AccountName
+    ManagedOrganizationalUnit = each.value.ManagedOrganizationalUnit
+    SSOUserEmail              = each.value.SSOUserEmail
+    SSOUserFirstName          = each.value.SSOUserFirstName
+    SSOUserLastName           = each.value.SSOUserLastName
     change_management_parameters = { M = {
       change_reason       =  lookup(var.change_management_parameters, "change_reason") 
       change_requested_by =  lookup(var.change_management_parameters, "change_requested_by") 
