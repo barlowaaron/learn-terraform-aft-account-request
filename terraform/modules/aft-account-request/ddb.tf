29ecea4ad8a88b9be72ab4e3b2a4e7251bacbe95
Aaron Barlow <aaron.barlow@progleasing.com>
2022-04-14 14:22:51 -0600

    adjust account customizations to remove null

--- terraform/modules/aft-account-request/ddb.tf
+++ terraform/modules/aft-account-request/ddb.tf
@@ -19,7 +19,7 @@ resource "aws_dynamodb_table_item" "account-request" {
       }
     }
     account_tags                = { S = jsonencode(var.account_tags) }
-    account_customizations_name = { S = var.account_customizations_name }
+    account_customizations_name = { S = each.value.account_customizations_name }
     custom_fields               = { S = jsonencode(var.custom_fields) }
   })
 }
