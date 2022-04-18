6e957f385fe9b7197bb2ffaf15cf331941c71f5f
Aaron Barlow <aaron.barlow@progleasing.com>
2022-04-18 12:58:52 -0600

    revert to something old

--- terraform/modules/aft-account-request/ddb.tf
+++ terraform/modules/aft-account-request/ddb.tf
@@ -2,24 +2,24 @@ resource "aws_dynamodb_table_item" "account-request" {
   table_name = var.account-request-table
   hash_key   = var.account-request-table-hash
     for_each = { for combo in var.account_assignment_map : combo.AccountEmail => combo}
-  item = <<ITEM
-   #jsonencode(
-     {
-    "id"                        = each.value.AccountEmail
-    AccountEmail              = each.value.AccountEmail
-    AccountName               = each.value.AccountName
-    ManagedOrganizationalUnit = each.value.ManagedOrganizationalUnit
-    SSOUserEmail              = each.value.SSOUserEmail
-    SSOUserFirstName          = each.value.SSOUserFirstName
-    SSOUserLastName           = each.value.SSOUserLastName
-    change_management_parameters = { M = {
-      change_reason       =  lookup(var.change_management_parameters, "change_reason") 
-      change_requested_by =  lookup(var.change_management_parameters, "change_requested_by") 
+  item = jsonencode({
+    id = { S = each.value.AccountEmail }
+    account_assignment_map = { M = {
+      AccountEmail              = { S = each.value.AccountEmail }
+      AccountName               = { S = each.value.AccountName }
+      ManagedOrganizationalUnit = { S = each.value.ManagedOrganizationalUnit }
+      SSOUserEmail              = { S = each.value.SSOUserEmail }
+      SSOUserFirstName          = { S = each.value.SSOUserFirstName }
+      SSOUserLastName           = { S = each.value.SSOUserLastName }
       }
     }
-    account_tags                = jsonencode(var.account_tags)
-    account_customizations_name = each.value.account_customizations_name
-    custom_fields               = jsonencode(var.custom_fields)
-  }
-ITEM
+    change_management_parameters = { M = {
+      change_reason       = { S = lookup(var.change_management_parameters, "change_reason") }
+      change_requested_by = { S = lookup(var.change_management_parameters, "change_requested_by") }
+      }
+    }
+    account_tags                = { S = jsonencode(var.account_tags) }
+    account_customizations_name = { S = each.value.account_customizations_name }
+    custom_fields               = { S = jsonencode(var.custom_fields) }
+  })
 }
