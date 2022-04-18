99acd6c809c4a72b5e7971764d01df3712afa652
Aaron Barlow <aaron.barlow@progleasing.com>
2022-04-18 11:38:58 -0600

    bracket each value

--- terraform/modules/aft-account-request/ddb.tf
+++ terraform/modules/aft-account-request/ddb.tf
@@ -3,20 +3,20 @@ resource "aws_dynamodb_table_item" "account-request" {
   hash_key   = var.account-request-table-hash
     for_each = { for combo in var.account_assignment_map : combo.AccountEmail => combo}
   item = jsonencode({
-    id = each.value.AccountEmail
-    AccountEmail              = each.value.AccountEmail
-    AccountName               = each.value.AccountName
-    ManagedOrganizationalUnit = each.value.ManagedOrganizationalUnit
-    SSOUserEmail              = each.value.SSOUserEmail
-    SSOUserFirstName          = each.value.SSOUserFirstName
-    SSOUserLastName           = each.value.SSOUserLastName
+    id                        = [each.value].AccountEmail
+    AccountEmail              = [each.value].AccountEmail
+    AccountName               = [each.value].AccountName
+    ManagedOrganizationalUnit = [each.value].ManagedOrganizationalUnit
+    SSOUserEmail              = [each.value].SSOUserEmail
+    SSOUserFirstName          = [each.value].SSOUserFirstName
+    SSOUserLastName           = [each.value].SSOUserLastName
     change_management_parameters = { M = {
       change_reason       =  lookup(var.change_management_parameters, "change_reason") 
       change_requested_by =  lookup(var.change_management_parameters, "change_requested_by") 
       }
     }
     account_tags                = jsonencode(var.account_tags)
-    account_customizations_name = each.value.account_customizations_name
+    account_customizations_name = [each.value].account_customizations_name
     custom_fields               = jsonencode(var.custom_fields)
   })
 }
